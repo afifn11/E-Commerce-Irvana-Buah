1d7501c432fca115e77de2b9e13640bd7332e1ce
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2026-03-13 14:32:42 +0700

    fix: register Brevo mail transport manually

--- Irvana-Buah/app/Providers/AppServiceProvider.php
+++ Irvana-Buah/app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,16 @@ class AppServiceProvider extends ServiceProvider
 
         Paginator::defaultView('vendor.pagination.custom');
         Paginator::defaultSimpleView('vendor.pagination.custom');
+
+        // Register Brevo mail transport
+        Mail::extend('brevo', function () {
+            return (new BrevoTransportFactory())->create(
+                new Dsn(
+                    'brevo+api',
+                    'default',
+                    config('mail.mailers.brevo.key')
+                )
+            );
+        });
     }
 }
